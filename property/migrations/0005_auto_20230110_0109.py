a0ad6a8a59d9223affdb02066877ee6db7c3aeb3
ZHartem21 <zhartem21@gmail.com>
2023-01-13 19:31:48 +0300

    Rewrite migration to use .update()

--- property/migrations/0005_auto_20230110_0109.py
+++ property/migrations/0005_auto_20230110_0109.py
@@ -5,13 +5,7 @@ from django.db import migrations
 
 def new_building_check_and_update(apps, schema_editor):
     Flat = apps.get_model('property', 'Flat')
-    for flat in Flat.objects.all():
-        if flat.construction_year >= 2015:
-            flat.new_building = True
-            flat.save()
-        if flat.construction_year < 2015:
-            flat.new_building = False
-            flat.save()
+    Flat.objects.filter(construction_year__gte=2015).update(new_building=True)
 
 
 class Migration(migrations.Migration):
