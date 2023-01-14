f5bae97e077703f99c28b1a5a1721428a6e4e0a0
ZHartem21 <zhartem21@gmail.com>
2023-01-14 19:38:49 +0300

    Fix migration for new building update

--- property/migrations/0005_auto_20230110_0109.py
+++ property/migrations/0005_auto_20230110_0109.py
@@ -6,6 +6,7 @@ from django.db import migrations
 def new_building_check_and_update(apps, schema_editor):
     Flat = apps.get_model('property', 'Flat')
     Flat.objects.filter(construction_year__gte=2015).update(new_building=True)
+    Flat.objects.filter(construction_year__lt=2015).update(new_building=False)
 
 
 class Migration(migrations.Migration):
